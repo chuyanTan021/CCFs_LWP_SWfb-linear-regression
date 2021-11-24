6f13029c180ed183cddda7eccccc9dd6cb56800c
chuyanTan021 <cyldwf123@gmail.com>
2021-11-24 04:52:12 -0700

    run different GCMs data_ modules, first Commit to 'Emergent Constraint' works

---
+++ plots_test1/run_simple_cmip6.py
@@ -0,0 +1,137 @@
+# main Funcion
+
+import netCDF4
+from numpy import *
+import matplotlib.pyplot as plt
+import xarray as xr
+import PyNIO as Nio
+import pandas as pd
+import glob
+from scipy.stats import *
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from matplotlib.ticker import MaxNLocator
+from matplotlib.colors import BoundaryNorm
+
+from area_mean import *
+from binned_cyFunctions5 import *
+from read_hs_file import read_var_mod
+
+
+from get_LWPCMIP6data import *
+from get_annual_so import *
+from calc_LRM_metrics import *
+
+
+
+def main():
+    #exp = 'historical'
+    #exp = 'piControl'
+    #exp = 'amip'
+    exp = 'abrupt-4xCO2'
+    SAM0={'modn': 'SAM0-UNICON', 'consort': 'SNU', 'cmip': 'cmip6',
+                'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    GISSE21G = {'modn': 'GISS-E2-1-G', 'consort': 'NASA-GISS', 'cmip': 'cmip6',
+                'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    CESM2 = {'modn': 'CESM2', 'consort': 'NCAR', 'cmip': 'cmip6',
+             'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+
+    if (exp=='piControl')|(exp=='amip'):
+        #GISSE21G = {'modn': 'GISS-E2-1-G', 'consort': 'NASA-GISS', 'cmip': 'cmip6',
+        #        'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+        #CESM2 = {'modn': 'CESM2', 'consort': 'NCAR', 'cmip': 'cmip6',
+        #     'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+        HADGEM3 = {'modn': 'HadGEM3-GC31-LL', 'consort': 'MOHC', 'cmip': 'cmip6',
+               'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+        MIROC6 = {'modn': 'MIROC6', 'consort': 'MIROC', 'cmip': 'cmip6',
+              'exper': exp, 'ensmem': 'r2i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+
+    CESM2FV2 = {'modn': 'CESM2-FV2', 'consort': 'NCAR', 'cmip': 'cmip6',
+             'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+
+    GISSE21H = {'modn': 'GISS-E2-1-H', 'consort': 'NASA-GISS', 'cmip': 'cmip6',
+                'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+
+    CESM2WACCM = {'modn': 'CESM2-WACCM', 'consort': 'NCAR', 'cmip': 'cmip6',
+             'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    HADGEM3 = {'modn': 'HadGEM3-GC31-LL', 'consort': 'MOHC', 'cmip': 'cmip6',
+               'exper': exp, 'ensmem': 'r1i1p1f3', 'gg': 'gn', "typevar": 'Amon'}
+    UKESM10 = {'modn': 'UKESM1-0-LL', 'consort': 'MOHC', 'cmip': 'cmip6',
+               'exper': exp, 'ensmem': 'r1i1p1f2', 'gg': 'gn', "typevar": 'Amon'}
+    IPSLCM6ALR = {'modn': 'IPSL-CM6A-LR', 'consort': 'IPSL', 'cmip': 'cmip6',
+                  'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gr', "typevar": 'Amon'}
+    CNRMCM6 = {'modn': 'CNRM-CM6-1', 'consort': 'CNRM-CERFACS', 'cmip': 'cmip6',
+               'exper': exp, 'ensmem': 'r1i1p1f2', 'gg': 'gr', "typevar": 'Amon'}
+    
+    CNRMESM2 = {'modn': 'CNRM-ESM2-1', 'consort': 'CNRM-CERFACS', 'cmip': 'cmip6', 
+               'exper': exp, 'ensmem': 'r1i1p1f2', 'gg': 'gr', "typevar": 'Amon'}
+    MIROC6 = {'modn': 'MIROC6', 'consort': 'MIROC', 'cmip': 'cmip6',
+              'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    MIROCES2L= {'modn': 'MIROC-ES2L', 'consort': 'MIROC', 'cmip': 'cmip6',
+              'exper': exp, 'ensmem': 'r1i1p1f2', 'gg': 'gn', "typevar": 'Amon'}#
+
+
+    GFDLCM4 = {'modn': 'GFDL-CM4', 'consort': 'NOAA-GFDL', 'cmip': 'cmip6',
+               'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gr1', "typevar": 'Amon'}
+    BCCESM1 = {'modn': 'BCC-ESM1', 'consort': 'BCC', 'cmip': 'cmip6',
+               'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    ACCESSCM2 = {'modn': 'ACCESS-CM2', 'consort': 'CSIRO-ARCCSS', 'cmip': 'cmip6',
+                 'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}#..'/glade/' don't have 'Amon' typevar
+    CAMSCSM1 = {'modn': 'CAMS-CSM1-0', 'consort': 'CAMS', 'cmip': 'cmip6',
+                'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    MPIESM12LR = {'modn': 'MPI-ESM1-2-LR', 'consort': 'MPI-M', 'cmip': 'cmip6',
+                  'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}#..'/glade/' dont have 'Amon' typevar
+    CanESM5 = {'modn': 'CanESM5', 'consort': 'CCCma', 'cmip': 'cmip6',
+               'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    INM_CM48 = {'modn': 'INM-CM4-8', 'consort': 'INM', 'cmip': 'cmip6',
+                'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gr1', "typevar": 'Amon'}#..'/glade/' dont have 'Amon' typevar
+    INM_CM50 = {'modn': 'INM-CM5-0', 'consort': 'INM', 'cmip': 'cmip6',
+                'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gr1', "typevar": 'Amon'}#..'/glade/' dont have 'Amon' typevar
+
+    NORESM2LM = {'modn': 'NorESM2-LM', 'consort': 'NCC', 'cmip': 'cmip6',
+                 'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    E3SM10 = {'modn': 'E3SM-1-0', 'consort': 'E3SM-Project', 'cmip': 'cmip6',
+              'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gr', "typevar": 'Amon'}
+    AWICM11MR = {'modn': 'AWI-CM-1-1-MR', 'consort': 'AWI', 'cmip': 'cmip6',
+                 'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}#..'/glade/' dont have 'tas' variable
+    CMCC = {'modn': 'CMCC-CM2-SR5', 'consort': 'CMCC', 'cmip': 'cmip6',
+            'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'} #..'/glade/' dont have 'abrupt-4xCO2'/'piControl' exper
+    ECE = {'modn': 'EC-Earth3', 'consort': 'EC-Earth-Consortium', 'cmip': 'cmip6',
+           'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'} #..dont have 'Amon' or variable 'tas'
+    #ECE has different variants for exper
+    ECEV = {'modn': 'EC-Earth3-Veg', 'consort': 'EC-Earth-Consortium', 'cmip': 'cmip6',
+           'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gr', "typevar": 'Amon'}#..too hard to operateee
+    
+    #ECEV has very discrete year in 'abrupt-4xCO2', semms continued but each in one yr file in 'piControl'
+    MRIESM20 = {'modn': 'MRI-ESM2-0', 'consort': 'MRI', 'cmip': 'cmip6',
+                'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}
+    FGOALSg3 = {'modn': 'FGOALS-g3', 'consort': 'CAS', 'cmip': 'cmip6',
+                'exper': exp, 'ensmem': 'r1i1p1f1', 'gg': 'gn', "typevar": 'Amon'}# '/glade/' dont have 'Amon' typever in 'abru4x'
+
+    #deck = [ACCESSCM2, AWICM11MR, BCCESM1, CESM2, CESM2FV2, CESM2WACCM, CNRMESM2, CAMSCSM1, CNRMCM6, ECE, ECEV, E3SM10, GFDLCM4, GISSE21H, GISSE21G, HADGEM3, INM_CM48, IPSLCM6ALR, MRIESM20, MIROC6, MPIESM12LR, NORESM2LM, SAM0 ]   #..current #23, Total in #29
+    #deck  = [BCCESM1, CanESM5, CESM2, CESM2FV2, CESM2WACCM, CAMSCSM1, CNRMCM6,CNRMESM2, E3SM10, GFDLCM4, GISSE21H, GISSE21G, HADGEM3, UKESM10, IPSLCM6ALR, MRIESM20, MIROC6, NORESM2LM, SAM0]   #..current #19
+    
+    
+    
+    deck  = [CanESM5, CESM2, CESM2FV2, CESM2WACCM, CAMSCSM1, CNRMCM6,CNRMESM2, E3SM10, GFDLCM4, GISSE21H, GISSE21G, HADGEM3, UKESM10, IPSLCM6ALR, MRIESM20, MIROC6, NORESM2LM, SAM0]   #..current #18
+    
+    # SOMETHING WRONG WITH FGOALS AND CMCC (in SWCRE in CMCC)
+    
+    MP = '/glade/collections/cmip/CMIP6/'
+    
+    for i in range(len(deck)):
+        DP =  MP + 'CMIP' + '/'+deck[i]['consort']+'/'+ deck[i]['modn'] + '/'+ deck[i]['exper'] \
+            + '/'+ deck[i]['ensmem'] +'/'+ deck[i]['typevar'] + '/' + 'tas' + '/'+ deck[i]['gg'] + '/'
+        if len(glob.glob(DP+ '*/*/'))!=0:
+            #print('number of run stored in glade', len(glob.glob(DP+ '*/*/')))
+            print('oh!', 'we have data in model: ', deck[i]['modn'])
+            try:
+                calc_LRM_metrics(**deck[i])
+                print('done ', i)
+            except:
+                print('fail to call function : calc_LRM_metrics', deck[i])
+
+
+if __name__== "__main__":
+    main()
+    
