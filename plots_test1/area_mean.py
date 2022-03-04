1f2f17045ac4e388a0ee4a8f12eabb4e6ced615c
chuyanTan021 <cyldwf123@gmail.com>
2022-03-04 16:10:44 -0700

    Updated on March 4th, hopefully i save the new code for asZelinka PLot

--- plots_test1/area_mean.py
+++ plots_test1/area_mean.py
@@ -1,8 +1,9 @@
 #..S array has 3 dimensions: (times, lat, lon)
 
+
 import numpy as np
 import matplotlib.pyplot as plt
-#import xarray as xr
+from xarray import DataArray
 #import PyNIO as Nio
 import pandas as pd
 
@@ -11,6 +12,7 @@ from sklearn import linear_model
 
 from sklearn.metrics import mean_squared_error, r2_score
 #from read_hs_file import read_var_mod
+
 
 
 def area_mean(S, lats, lons):
@@ -36,3 +38,103 @@ def area_mean(S, lats, lons):
     
     
     return GMT
+def area_grid(lat, lon):
+    """
+    Calculate the area of each grid cell
+    Area is in square meters
+    
+    Input
+    -----------
+    lat: vector of latitude in degrees
+    lon: vector of longitude in degrees
+    
+    Output
+    -----------
+    area: grid-cell area in square-meters with dimensions, [lat,lon]
+    
+    Notes
+    -----------
+    Based on the function in
+    https://github.com/chadagreene/CDT/blob/master/cdt/cdtarea.m
+    """
+    
+    
+
+    xlon, ylat = np.meshgrid(lon, lat)
+    R = earth_radius(ylat)
+
+    dlat = np.deg2rad(np.gradient(ylat, axis=0))
+    dlon = np.deg2rad(np.gradient(xlon, axis=1))
+
+    dy = dlat * R
+    dx = dlon * R * np.cos(np.deg2rad(ylat))
+
+    area = dy * dx
+
+    xda = DataArray(
+        area,
+        dims=["latitude", "longitude"],
+        coords={"latitude": lat, "longitude": lon},
+        attrs={
+            "long_name": "area_per_pixel",
+            "description": "area per pixel",
+            "units": "m^2",
+        },
+    )
+    return xda
+
+
+
+def earth_radius(lat):
+    '''
+    calculate radius of Earth assuming oblate spheroid
+    defined by WGS84
+    
+    Input
+    ---------
+    lat: vector or latitudes in degrees  
+    
+    Output
+    ----------
+    r: vector of radius in meters
+    
+    Notes
+    -----------
+    WGS84: https://earth-info.nga.mil/GandG/publications/tr8350.2/tr8350.2-a/Chapter%203.pdf
+    '''
+    
+
+    # define oblate spheroid from WGS84
+    a = 6378137
+    b = 6356752.3142
+    e2 = 1 - (b**2/a**2)
+    
+    # convert from geodecic to geocentric
+    # see equation 3-110 in WGS84
+    lat = np.deg2rad(lat)
+    lat_gc = np.arctan( (1-e2)*np.tan(lat) )
+
+    # radius equation
+    # see equation 3-107 in WGS84
+    r = (
+        (a * (1 - e2)**0.5) 
+         / (1 - (e2 * np.cos(lat_gc)**2))**0.5 
+        )
+
+    return r
+
+    '''
+    How to use the above code(Feb.26th, 2022 Added): 
+    ###  "ds" is the data he used in this example, read by 'xr.open_dataset()' method
+    
+    
+    # area dataArray 
+    da_area = area_grid(ds['latitude'], ds['longitude'])
+    # 总面积
+    total_area = da_area.sum(['latitude','longitude'])
+    # 由网格单元面积加权的温度
+    temp_weighted = (ds['temperature']*da_area) / total_area
+    
+    # 面积加权平均温度
+    temp_weighted_mean = temp_weighted.sum(['latitude','longitude'])
+    '''
